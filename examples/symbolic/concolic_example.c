eca4e081858c1d0816868f11a39dca2f8e49e096
Marcell Haritopoulos <marcellharitopoulos@gmail.com>
2020-10-28 11:25:15 +0100

    conquerer: Add loop to concolic_example.c

--- examples/symbolic/concolic_example.c
+++ examples/symbolic/concolic_example.c
@@ -6,18 +6,21 @@ uint64_t main() {
   uint64_t* x3;
   uint64_t* x4;
   uint64_t* x5;
+  uint64_t* x6;
 
   x1 = malloc(8);
   x2 = malloc(8);
   x3 = malloc(8);
   x4 = malloc(8);
   x5 = malloc(8);
+  x6 = malloc(8);
 
   read(0, x1, 8);
   read(0, x2, 8);
   read(0, x3, 8);
   read(0, x4, 8);
   read(0, x5, 8);
+  read(0, x6, 8);
 
   if (*x1 == 1) {
     exit(1);
@@ -42,4 +45,15 @@ uint64_t main() {
   if ((*x1 + 5) <= 5) {
     exit(1);
   }
+
+  while (*x6 < 20) {
+    *x6 = *x6 + 1;
+  }
+
+  if (*x6 > 20) {
+    if (*x6 < 20) {
+      exit(1);
+    }
+  }
+
 }
